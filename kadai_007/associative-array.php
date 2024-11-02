76065685f82039d3b173612a95f26691c8f6fb7c
kyurinoasaduke <asuka.ran.120421@gmail.com>
2024-11-02 23:30:56 +0900

    kadai_007アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>課題007</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $food_data =array('name' =>'onion','price' =>200,'weight' =>160);
+
+         print_r($food_data);
+
+
+
+         ?>
+     </p>
+ </body>
+ 
+ </html>
