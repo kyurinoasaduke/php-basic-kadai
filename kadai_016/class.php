3919e61cc3612dea5f0caaf1a36740af8fd3c8a5
kyurinoasaduke <asuka.ran.120421@gmail.com>
2024-11-11 15:03:17 +0900

    kadai_016アップロード

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -16,21 +16,23 @@
          private $name;
          private $price;
          
-         //メソッドを定義
-         public function show_price(int $price) {
-            echo $this->price = $price;
-         }
 
          //コンストラクタを定義する
          public function __construct (string $name, int $price) {
             $this->name = $name;
             $this->price = $price;
          }
+
+         //メソッドを定義
+         public function show_price() {
+            echo $this->price;
+         }
+
          }
 
          //インスタンス化
-         $food =new Food('potato','250');
-         print_r($food);
+         $potato = new Food('potato','250');
+         print_r($potato);
          echo'<br>';
 
          class Animal {
@@ -39,23 +41,24 @@
             private $height;
             private $weight;
 
-            public function show_height(int $height) {
-                 echo$this->height = $height;
+            public function __construct(string $name, int $height, int $weight) {
+                $this->name = $name; 
+                $this->height = $height;
+                $this->weight = $weight;
+        }     
+            public function show_height() {
+                 echo$this->height;
             }
 
-            public function __construct(string $name, int $height, int $weight) {
-                    $this->name = $name; 
-                    $this->height = $height;
-                    $this->weight = $weight;
-            }     
+            
         }
         
-        $animal= new Animal('dog', 60, 5000);
-        print_r($animal);
+        $dog = new Animal('dog', 60, 5000);
+        print_r($dog);
         echo '<br>';
-        $food->show_price(250);
+        $potato->show_price();
         echo '<br>';
-        $animal->show_height(60);
+        $dog->show_height();
         echo '<br>';
          
          ?>
